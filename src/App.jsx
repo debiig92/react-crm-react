bc894f5fe3fb0e41ddff8d5f10b0a9cecd6ecb5e
debiig92 <deby.guardado@gmail.com>
2022-04-30 16:48:37 -0600

    configurar rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import IniciarSesion from "./layout/IniciarSesion";
+import Layout from "./layout/Layout";
+import Inicio from "./paginas/Inicio";
+import LoginForm from "./paginas/LoginForm";
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IniciarSesion />}>
+          <Route index element={<LoginForm />} />
+        </Route>
+        <Route path="/clientes" element={<Layout />}>
+          <Route index element={<Inicio />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
