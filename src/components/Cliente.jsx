5aef0f30f937af7c005c5021f2df8331219bb28e
debiig92 <deby.guardado@gmail.com>
2022-05-07 22:24:35 -0600

    tabla de productos existentes

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Cliente = ({ cliente }) => {
+  return (
+    <tr className="border-b hover:bg-gray-100">
+      <td className="p-3">{cliente.nombre}</td>
+      <td className="p-3">
+        <p className="text-gray-800 uppercase font-bold">
+          Email: <span>{cliente.email}</span>
+        </p>
+        <p className="text-gray-800 uppercase font-bold">
+          Tel: <span>{cliente.telefono}</span>
+        </p>
+      </td>
+      <td className="p-3">{cliente.empresa}</td>
+      <td className="p-3">
+        <button
+          className="bg-yellow-600 hover:bg-yellow-700 block w-full text-white p-2 uppercase font-bold text-xs mb-2"
+          type="button"
+        >
+          Ver
+        </button>
+
+        <button
+          className="bg-blue-600 hover:bg-blue-700 block w-full text-white p-2 uppercase font-bold text-xs mb-2"
+          type="button"
+        >
+          Editar
+        </button>
+
+        <button
+          className="bg-red-600 hover:bg-red-700 block w-full text-white p-2 uppercase font-bold text-xs"
+          type="button"
+        >
+          Eliminar
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default Cliente;
--- src/paginas/Inicio.jsx
+++ src/paginas/Inicio.jsx
@@ -1,7 +1,42 @@
-import React from "react";
-
+import { useState, useEffect } from "react";
+import Cliente from "../components/Cliente";
 const Inicio = () => {
-  return <div>Inicio</div>;
+  const [clientes, setClientes] = useState([]);
+
+  useEffect(() => {
+    const obtenerClientes = async () => {
+      try {
+        const url = "http://localhost:4000/clientes";
+        const respuesta = await fetch(url);
+        setClientes(await respuesta.json());
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    obtenerClientes();
+  }, []);
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+      <p className="mt-3">Administra tus clientes</p>
+      <table className="w-full mt-5 table-auto shadow bg-white">
+        <thead className="bg-blue-800 text-white">
+          <tr>
+            <th className="p-2">Nombre</th>
+            <th className="p-2">Contacto</th>
+            <th className="p-2">Empresa</th>
+            <th className="p-2">Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {clientes.map((cliente) => (
+            <Cliente key={cliente.id} cliente={cliente} />
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default Inicio;
